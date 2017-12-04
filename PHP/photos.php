501ea8894e20dfa7cb6ed9f55b7a344680eb71d2
JonDunnett <dunnett_jonathan@wheatoncollege.edu>
2017-12-04 18:46:30 -0500

    fotofan stuff

--- PHP/photos.php
+++ PHP/photos.php
@@ -26,8 +26,21 @@ if ($_COOKIE["username"] == "joe") {
 	$request->success = true;
 } else {
 	echo json_encode($request);
+	exit();
 }
 
+while (! feof($db)) {
+	$photo = new Photo();
+	$iphoto  = json_decode(fgets($db));
+	$photo->loc   = $iphoto->loc;
+	$photo->path  = $iphoto->path;
+	$photo->year  = $iphoto->year;
+	$photo->desc  = $iphoto->desc;
+	$photo->count = $iphoto->count;
+	$photo->state = $iphoto->state;
+	array_push($request->photos,$photo);
+}
 
+echo json_encode($request);
 
 ?>
